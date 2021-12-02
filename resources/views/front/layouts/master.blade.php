b06f402139cfca7120d203e82f35c720071b22dd
ahmedkh19 <ahmed@thinkvolc.com>
2021-12-03 00:58:42 +0300

    =Edit in the master file

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -165,12 +165,10 @@
               </div>
 
               <div class="col-6 col-lg-2">
-                <h6 class="text-uppercase fw-600 mb-4">About</h6>
+                <h6 class="text-uppercase fw-600 mb-4">{{__('data.About')}}</h6>
                 <div class="nav flex-column">
-                  <a class="nav-link" href="#">Our team</a>
-                  <a class="nav-link" href="#">Careers</a>
-                  <a class="nav-link" href="#">Cookie Policy</a>
-                  <a class="nav-link" href="#">Privacy Policy</a>
+                  <a class="nav-link" href="{{ route('home') }}/#about">{{ __('data.Who we are') }}</a>
+                  <a class="nav-link" href="{{ asset('uploads/pdf/Terms&Privacy.pdf') }}">{{__('data.Privacy Policy')}}</a>
                   
                 </div>
               </div>
@@ -178,26 +176,23 @@
               <div class="col-6 col-lg-2">
                 <h6 class="text-uppercase fw-600 mb-4">Help</h6>
                 <div class="nav flex-column">
-                  <a class="nav-link" href="#">Contact Us</a>
-                  <a class="nav-link" href="#">Support</a>
-                  <a class="nav-link" href="#">Terms & conditions</a>
-                  <a class="nav-link" href="#">Refund policy</a>
+                  <a class="nav-link" href="{{ route('home') }}/#contact">{{ __('data.Contact Us') }}</a>
+                  <a class="nav-link" href="https://api.whatsapp.com/send?phone=966557990467&text={{__('data.I have a question')}}">{{__('data.Support')}}</a>
+                  <a class="nav-link" href="{{ asset('uploads/pdf/Terms&Privacy.pdf') }}">{{__('data.Terms & conditions')}}</a>
                 </div>
               </div>
               
               <div class="col col-lg-3 order-lg-last">
                 <div class="mb-6">
-                  <h6 class="text-uppercase fw-600 mb-4">Ways to pay</h6>
+                  <h6 class="text-uppercase fw-600 mb-4">{{__('data.Ways to pay')}}</h6>
                   <div class="text-light lead-5 lh-1">
-                    <a href="store.html" class="mr-2"><i class="fab fa-cc-paypal"></i></a>
-                    <a href="store.html" class="mr-2"><i class="fab fa-cc-visa"></i></a>
-                    <a href="store.html" class="mr-2"><i class="fab fa-cc-amazon-pay"></i></a>
-                    <a href="store.html" class="mr-2"><i class="fab fa-cc-stripe"></i></a>
-                    <a href="store.html" class="mr-2"><i class="fab fa-cc-jcb"></i></a>
+                    <a href="#" class="mr-2"><i class="fab fa-cc-paypal"></i></a>
+                    <a href="#" class="mr-2"><i class="fab fa-cc-visa"></i></a>
+                    <a href="#" class="mr-2"><i class="fab fa-cc-mastercard"></i></a>
                   </div>
                 </div>
                 <div>
-                  <h6 class="mb-2">Reviews</h6>
+                  <h6 class="mb-2">{{__('data.Reviews')}}</h6>
                   <div class="text-warning lead-1">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
